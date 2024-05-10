dfe14d8058a0b18cf6775849316fb278760b3312
yuvansankar-dev <148607533+yuvansankar-dev@users.noreply.github.com>
2024-05-10 13:49:03 +0530

    Update cors index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ import leaveRouter from "./Routers/leave.router.js";
 import holidayRouter from "./Routers/holiday.router.js";
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173', // Allow requests only from this origin
-  methods: ['GET', 'POST'], // Allow only these HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
-  credentials: true // Enable credentials (cookies) to be sent with requests
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true 
 }));
 app.use(express.json())
 configDotenv()
